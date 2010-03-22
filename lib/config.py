2e163995cd38321702864bb132d865a8a06c2bac
Marc <marcpare0@gmail.com>
2010-03-22 11:04:05 -0400

    cleaning up files

--- lib/config.py
+++
@@ -1,7 +0,0 @@
-ENVIRONMENT = "development"
-
-BEANSTALK_HOST = "localhost"
-BEANSTALK_PORT = "14711"
-
-GAMS_SERVER = "localhost"
-GAMS_PORT = 19913
--- www/config.py
+++
@@ -1,4 +0,0 @@
-ENVIRONMENT = "development"
-HOST = "localhost"
-PORT = "8080"
-ROOT_URL = "http://localhost:8080/"
